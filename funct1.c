68536c87e995ebea87ad0557e3369b0e9e9a6683
faith481 <faithayeni481@gmail.com>
2023-08-23 01:04:13 -0700

    functions1.c

--- funct1.c
+++ funct1.c
@@ -94,6 +94,6 @@ int _myhelp(info_t *info)
 	arg_array = info->argv;
 	_puts("help call works. Function not yet implemented \n");
 	if (0)
-		_puts(*arg_array); /* temp att_unused workaround */
+	_puts(*arg_array); /* temp att_unused workaround */
 	return (0);
 }
