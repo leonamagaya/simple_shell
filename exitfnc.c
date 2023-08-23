7396b91690ed79b8bd8c97fda32345803a6dd020
faith481 <faithayeni481@gmail.com>
2023-08-23 00:56:33 -0700

    exitfnc.c

--- exitfnc.c
+++ exitfnc.c
@@ -47,11 +47,11 @@ char *_strncat(char *dest, char *src, int n)
 	i = 0;
 	j = 0;
 	while (dest[i] != '\0')
-		i++;
+	i++;
 	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
-		i++;
+	i++;
 		j++;
 	}
 	if (j < n)
